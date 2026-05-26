4b9e414dfcdbc7a3268c862c1bd759529850c56f
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-05-26 18:14:28 -0300

    feat: implements strings

--- lista06/strings.cpp
+++ lista06/strings.cpp
@@ -4,15 +4,31 @@
 
 using namespace std;
 
-bool comparar(const string &a, const string& b) {
-    // TODO
-    return false;    
+bool comparar(const string &a, const string &b) {
+    return a.length() < b.length();
 }
 
 vector<vector<string>> agrupar(vector<string> &strs) {
     vector<vector<string>> res;
+    if (strs.empty()) return res;
 
-    // TODO
+    sort(strs.begin(), strs.end(), comparar);
+
+    vector<string> grupoAtual;
+    grupoAtual.push_back(strs[0]);
+
+    for (size_t i = 1; i < strs.size(); i++) {
+        if (strs[i].length() == grupoAtual[0].length()) {
+            grupoAtual.push_back(strs[i]);
+        } else {
+            res.push_back(grupoAtual);
+            grupoAtual.clear();
+            grupoAtual.push_back(strs[i]);
+        }
+    }
+    if (!grupoAtual.empty()) {
+        res.push_back(grupoAtual);
+    }
 
     return res;
 }
